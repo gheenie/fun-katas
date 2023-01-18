d63566e96c8f2b844b3bcf645fa42e67ca3c2d84
infoomics <sbx_94@hotmail.com>
2023-01-18 16:28:39 +0000

    second and third  test case

--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,9 +1,20 @@
 // Please do not change the name of this function
 function dnaPairs(dna) {
   // Write your code here
+  /*
   if (dna === '') {
     return [];
   }
+  */
+
+  const basePairs = {
+    A: 'T', 
+    T: 'A', 
+    G: 'C', 
+    C: 'G'
+  };
+
+  return dna.split('').map((base) => [base, basePairs[base]]);
 }
 
 module.exports = dnaPairs;
