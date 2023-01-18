85002289bff227aff618066ddc25aeb81524bd10
infoomics <sbx_94@hotmail.com>
2023-01-18 17:56:31 +0000

    first test case

--- get-tweet-data/get-tweet-data.js
+++ get-tweet-data/get-tweet-data.js
@@ -1,6 +1,15 @@
 // Please do not change the name of this function
 function getTweetData (tweet) {
   // Your code here
+  const tweetData = { 
+    tags: [], 
+    mentions: [], 
+    tagCount: 0, 
+    mentionCount: 0, 
+    length: tweet.length
+  };
+
+  return tweetData;
 };
 
 module.exports = getTweetData;
