631a67d56121c942a1e7d7eec3ecd847a1d94cd3
infoomics <sbx_94@hotmail.com>
2023-01-18 16:29:55 +0000

    initial completion of tests

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,7 +1,33 @@
 const dnaPairs = require('../dna-pairs.js');
 
 describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
+  test('empty string', () => {
     // add your test assertion(s) here
+    const input = '';
+
+    const output = dnaPairs(input);
+
+    const expected = [];
+    expect(output).toEqual(expected);
+  });
+
+  test('one base', () => {
+    // add your test assertion(s) here
+    const input = 'G';
+
+    const output = dnaPairs(input);
+
+    const expected = [['G', 'C']];
+    expect(output).toEqual(expected);
+  });
+
+  test('four different bases', () => {
+    // add your test assertion(s) here
+    const input = 'ATCG';
+
+    const output = dnaPairs(input);
+
+    const expected = [['A', 'T'], ['T', 'A'], ['C', 'G'], ['G', 'C']];
+    expect(output).toEqual(expected);
   });
 });
