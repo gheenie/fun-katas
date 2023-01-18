2a8dbe94f1365342e5541da93717ca51609c4785
infoomics <sbx_94@hotmail.com>
2023-01-18 20:03:36 +0000

    initial completion of test cases

---
+++ update-remote-students/__tests__/update-remote-students.test.js
@@ -0,0 +1,38 @@
+const updateRemoteStudents = require('../update-remote-students');
+
+describe('updateRemoteStudents()', () => {
+    test('Original input not mutated', () => {
+        const input = [{ name: 'Euler', age: 27 }];
+
+        const output = updateRemoteStudents(input);
+
+        const expected = [{ name: 'Euler', age: 27 }];
+        expect(input).toEqual(expected);
+    });
+
+    test('one student with no location', () => {
+        const input = [{ name: 'Euler', age: 27 }];
+
+        const output = updateRemoteStudents(input);
+
+        const expected = [{ name: 'Euler', age: 27, location: 'remote' }];
+        expect(output).toEqual(expected);
+    });
+
+    test('one student with no location, some with locations', () => {
+        const input = [
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22 },
+            { name: 'Tao', age: 47, location: 'manchester' }
+        ];
+
+        const output = updateRemoteStudents(input);
+
+        const expected = [
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22, location: 'remote' },
+            { name: 'Tao', age: 47, location: 'manchester' }
+        ];
+        expect(output).toEqual(expected);
+    });
+});
