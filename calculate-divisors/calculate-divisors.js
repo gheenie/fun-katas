4e5575f4bf6ed602fcb35b78dd2fe311d234590f
infoomics <sbx_94@hotmail.com>
2023-01-18 19:26:14 +0000

    first test case, also completes the rest

--- calculate-divisors/calculate-divisors.js
+++ calculate-divisors/calculate-divisors.js
@@ -1,4 +1,16 @@
 // Please do not change the name of this function
 function calculateDivisors(num) {
   // Your code here
+
+  const countMultiplesOf3 = Math.floor((num - 1) / 3);
+ 
+  const countMultiplesOf5 = Math.floor((num - 1) / 5);
+
+  function nthTriangleNumber(n, firstNum = 1, lastNum = n) {
+    return n * (firstNum + lastNum) / 2;
+  }
+
+  return 3 * nthTriangleNumber(countMultiplesOf3) + 5 * nthTriangleNumber(countMultiplesOf5);
 }
+
+module.exports = calculateDivisors;
