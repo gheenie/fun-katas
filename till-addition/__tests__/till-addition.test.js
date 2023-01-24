edafb4721b2eea11373ae10b3f12186409b00be6
infoomics <sbx_94@hotmail.com>
2023-01-24 19:43:12 +0000

    first completion of tests and test cases

--- till-addition/__tests__/till-addition.test.js
+++ till-addition/__tests__/till-addition.test.js
@@ -1,12 +1,30 @@
-const funcName = require('kata');
+const tillAddition = require('../till-addition');
 
 describe('', () => {
-    test('', () => {
-        const input = undefined;
+    test('pence only, same value', () => {
+        const input = { "1p": 1, "2p": 1 };
 
-        const output = funcName(input);
+        const output = tillAddition(input);
 
-        const expected = undefined;
-        expect(output).to(expected);
+        const expected = '£0.03';
+        expect(output).toBe(expected);
+    });
+
+    test('pence only, diff value', () => {
+        const input = { "1p": 1, "2p": 1, "5p": 1, "10p": 3, "20p": 1 };
+
+        const output = tillAddition(input);
+
+        const expected = '£0.58';
+        expect(output).toBe(expected);
+    });
+
+    test('pence and £, diff value', () => {
+        const input = { "5p": 1, "10p": 1, "20p": 1, "50p": 1, "£1": 3, "£50": 2 };
+
+        const output = tillAddition(input);
+
+        const expected = '£103.85';
+        expect(output).toBe(expected);
     });
 });
--- till-addition/till-addition.js
+++ till-addition/till-addition.js
@@ -1,6 +1,48 @@
+function roundTo2Decimals(num) {
+  return +(Math.round(num + "e+2")  + "e-2");
+}
+
 // Please do not change the name of this function
 function tillAddition (cash) {
   // Your code here
+
+  const till = {
+    "1p": 0,
+    "2p": 0,
+    "5p": 0,
+    "10p": 0,
+    "20p": 0,
+    "50p": 0,
+    "£1": 0,
+    "£2": 0,
+    "£5": 0,
+    "£10": 0,
+    "£20": 0,
+    "£50": 0
+  };
+
+  for ( const [denomination, count] of Object.entries(cash) ) {
+    till[denomination] += count;
+  }
+
+  let total = 0;
+
+  for ( const [denomination, count] of Object.entries(till) ) {
+    let faceValue = 0;
+    
+    if ( denomination.endsWith('p') ) {
+      faceValue = Number( denomination.slice(0, -1) ) / 100;
+    } else {
+      faceValue = Number( denomination.substring(1) );
+    }
+
+    total += faceValue * count;
+  }
+
+  // floating point imprecision
+  total = roundTo2Decimals(total);
+
+  return `£${total}`;
 };
 
 module.exports = tillAddition;
