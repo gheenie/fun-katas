68d10f6856f76c825e6e74eeab12bc96f0f251fc
infoomics <sbx_94@hotmail.com>
2023-01-24 20:25:51 +0000

    first completion of test cases

--- change-calculator/change-calculator.js
+++ change-calculator/change-calculator.js
@@ -1,6 +1,22 @@
 // Please do not change the name of this function
 function changeCalculator (num) {
   // Your code here
+
+  const change = {};
+  const denominators = [50, 20, 10, 5, 2];
+
+  denominators.forEach( (denominator) => {
+    change[denominator] = Math.trunc(num / denominator);
+    num -= change[denominator] * denominator;
+  } );
+
+  change[1] = num;
+
+  for ( const [denominator, count] of Object.entries(change) ) {
+    if (count === 0) delete change[denominator];
+  }
+
+  return change;
 };
 
 module.exports = changeCalculator;
