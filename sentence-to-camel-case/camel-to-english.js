8e3c6bebd2c8d72ccf30ec57eb6db47dc9d4c5dc
infoomics <sbx_94@hotmail.com>
2023-01-25 09:26:14 +0000

    removed unnecessary code

--- sentence-to-camel-case/camel-to-english.js
+++ sentence-to-camel-case/camel-to-english.js
@@ -5,16 +5,6 @@ function camelToEnglish(camelCasedSentence) {
   const englishSentence = camelCasedSentence.replace(/([A-Z])/g, ' $1').trim();
 
   return englishSentence[0].toUpperCase() + englishSentence.substring(1).toLowerCase() + '.';
-  
-  const wordsUpperCCed = words.map( (word) => {
-    return word[0].toUpperCase() + word.substring(1).toLowerCase();
-  } );
-
-  if (!shouldUpperCC) {
-    wordsUpperCCed[0] = wordsUpperCCed[0][0].toLowerCase() + wordsUpperCCed[0].substring(1);
-  }
-
-  return wordsUpperCCed.join('');
 };
 
 module.exports = camelToEnglish;
