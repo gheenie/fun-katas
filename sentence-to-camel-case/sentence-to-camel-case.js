e279e1d5ded080f72b0bdfcad216dfbb3546f457
infoomics <sbx_94@hotmail.com>
2023-01-25 01:35:11 +0000

    first completion of tests and test cases

--- sentence-to-camel-case/sentence-to-camel-case.js
+++ sentence-to-camel-case/sentence-to-camel-case.js
@@ -1,6 +1,18 @@
 // Please do not change the name of this function
-function sentenceToCamelCase (sentence) {
+function sentenceToCamelCase (sentence, shouldUpperCC) {
   // Your code here
+  
+  const words = sentence.split(' ');
+
+  const wordsUpperCCed = words.map( (word) => {
+    return word[0].toUpperCase() + word.substring(1).toLowerCase();
+  } );
+
+  if (!shouldUpperCC) {
+    wordsUpperCCed[0] = wordsUpperCCed[0][0].toLowerCase() + wordsUpperCCed[0].substring(1);
+  }
+
+  return wordsUpperCCed.join('');
 };
 
 module.exports = sentenceToCamelCase;
