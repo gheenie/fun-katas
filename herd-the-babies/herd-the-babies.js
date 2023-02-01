6ef708ada6ecc63f8b0614668cbc101d60a77c90
infoomics <sbx_94@hotmail.com>
2023-02-01 19:13:05 +0000

    replaced with full code for all tests

--- herd-the-babies/herd-the-babies.js
+++ herd-the-babies/herd-the-babies.js
@@ -2,7 +2,26 @@
 function herdTheBabies (str) {
   // Your code here
 
-  return str.split('').sort().join('');
+  const letterTable = {
+    'A': 0,
+    'a': 0,
+    'B': 0,
+    'b': 0,
+    'C': 0,
+    'c': 0,
+    'D': 0,
+    'd': 0,
+  }
+
+  str.split('').forEach( letter => letterTable[letter] += 1 );
+
+  let buildingString = '';
+
+  for ( const [letter, count] of Object.entries(letterTable) ) {
+    for (let i = 0; i < count; i++) buildingString += letter;
+  }
+
+  return buildingString;
 };
 
 module.exports = herdTheBabies;
