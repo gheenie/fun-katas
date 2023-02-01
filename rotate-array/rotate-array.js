f92dbbd01c987e1fb7ff20fd655d331167eadd64
infoomics <sbx_94@hotmail.com>
2023-02-01 18:08:21 +0000

    initial adding code for all tests

--- rotate-array/rotate-array.js
+++ rotate-array/rotate-array.js
@@ -1,6 +1,19 @@
 // Please do not change the name of this function
-function rotateArray (arr, num) {
+function rotateArray (inputArr, num) {
   // Your code here
+
+  const arr = JSON.parse( JSON.stringify(inputArr) );
+
+  // Not strictly necessary.
+  if (num === 0) return arr;
+
+  let numOfFrontItems = 0;
+  if (num > 0) numOfFrontItems = arr.length - num;
+  if (num < 0) numOfFrontItems = num * -1;
+
+  const frontOfArr = arr.splice(0, numOfFrontItems);
+  
+  return [...arr, ...frontOfArr];
 };
 
 module.exports =  rotateArray;
