7bc1b9d25fc0d947b288302359f05a86db3ad29d
infoomics <sbx_94@hotmail.com>
2023-02-01 16:13:35 +0000

    added tests

---
+++ pig-latin/pig-latin.test.js
@@ -0,0 +1,53 @@
+const pigLatin = require('./pig-latin');
+
+describe('', () => {
+    test('empty string', () => {
+        const input = '';
+
+        const output = pigLatin(input);
+
+        const expected = '';
+
+        expect(output).toBe(expected);
+    });
+
+    test('1 consonant', () => {
+        const input = 'northcoders';
+
+        const output = pigLatin(input);
+
+        const expected = 'orthcodersnay';
+        
+        expect(output).toBe(expected);
+    });
+
+    test('>1 consonant', () => {
+        const input = 'sheffield';
+
+        const output = pigLatin(input);
+
+        const expected = 'effieldshay';
+        
+        expect(output).toBe(expected);
+    });
+
+    test('vowel', () => {
+        const input = 'algorithm';
+
+        const output = pigLatin(input);
+
+        const expected = 'algorithmway';
+        
+        expect(output).toBe(expected);
+    });
+
+    test('multiple words', () => {
+        const input = 'keep on coding';
+
+        const output = pigLatin(input);
+
+        const expected = 'eepkay onway odingcay';
+        
+        expect(output).toBe(expected);
+    });
+});
