b25c62e25d48d521fecad41bf5541a803ac320af
infoomics <sbx_94@hotmail.com>
2023-02-01 16:27:21 +0000

    added code for all tests

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -1,6 +1,20 @@
 // Please do not change the name of this function
 function pigLatin(str) {
   // Your code here
+
+  if (str === '') return '';
+
+  if (str.indexOf(' ') === -1) {
+    if ( /^[AEIOUaeiou]/.test(str) ) return str + 'way';
+
+    const frontConsonants = str.match(/^[^AEIOUaeiou]+/)[0];
+    
+    return str.substring(frontConsonants.length) + frontConsonants + 'ay';
+  }
+
+  const splitWords = str.split(' ');
+
+  return splitWords.map(word => pigLatin(word)).join(' ');
 };
 
 module.exports = pigLatin
