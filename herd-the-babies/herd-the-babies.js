4660938cbe52f3bc0c61e8683cfcc608fde4149b
infoomics <sbx_94@hotmail.com>
2023-02-01 18:22:51 +0000

    initial half-completed code

--- herd-the-babies/herd-the-babies.js
+++ herd-the-babies/herd-the-babies.js
@@ -1,6 +1,8 @@
 // Please do not change the name of this function
 function herdTheBabies (str) {
   // Your code here
+
+  return str.split('').sort().join('');
 };
 
 module.exports = herdTheBabies;
